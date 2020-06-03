79ec5c2504f466d44b596e00a230538b64ca5102
yanzms <www.374011746@qq.com>
2020-06-04 02:18:35 +0800

    238 The type Product except self

---
+++ src/main/java/org/stray/leet/code/ProductExceptSelf.java
@@ -0,0 +1,61 @@
+package org.stray.leet.code;
+
+/**
+ * 238
+ * The type Product except self.
+ *
+ * @author yanzm
+ * @since 2020 /6/4
+ */
+public class ProductExceptSelf {
+    /**
+     * Product except self int [ ].
+     *
+     * @param nums the nums
+     * @return the int [ ]
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return nums;
+        }
+        int[] reverse = new int[length];
+        int[] order = new int[length];
+        int orderMulNum = 1;
+        int reverseMulNum = 1;
+        for (int i = 0; i < length; ++i) {
+            order[i] = orderMulNum;
+            orderMulNum *= nums[i];
+            reverse[length - 1 - i] = reverseMulNum;
+            reverseMulNum *= nums[length - 1 - i];
+        }
+        for (int i = 0; i < length; ++i) {
+            order[i] *= reverse[i];
+        }
+        return order;
+    }
+
+    public int[] productExceptSelfV2(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return nums;
+        }
+        int[] result = new int[length];
+        int orderMulNum = 1;
+        int reverseMulNum = 1;
+        for (int i = 0; i < length; ++i) {
+            if (length - 1 - i < i) {
+                result[i] *= orderMulNum;
+                result[length - 1 - i] *= reverseMulNum;
+            } else if (length - 1 - i == i) {
+                result[i] = orderMulNum * reverseMulNum;
+            } else {
+                result[i] = orderMulNum;
+                result[length - 1 - i] = reverseMulNum;
+            }
+            orderMulNum *= nums[i];
+            reverseMulNum *= nums[length - 1 - i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/stray/leet/code/ProductExceptSelfTest.java
@@ -0,0 +1,20 @@
+package org.stray.leet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author yanzm
+ * @since 2020/6/4
+ */
+class ProductExceptSelfTest {
+
+    private final ProductExceptSelf exceptSelf=new ProductExceptSelf();
+
+    @Test
+    void productExceptSelfV2() {
+        int[] result = exceptSelf.productExceptSelfV2(new int[]{1, 2, 3, 4});
+        assertArrayEquals(result, new int[]{24, 12, 8, 6});
+    }
+}
