ca8e5e0b5c3b2a0a2e6b0dc019c41284195ff416
yanzms <www.374011746@qq.com>
2020-06-09 23:39:45 +0800

    面试题46. 把数字翻译成字符串

---
+++ src/main/java/org/stray/leet/code/offer/TranslateNum.java
@@ -0,0 +1,29 @@
+package org.stray.leet.code.offer;
+
+/**
+ * 面试题46. 把数字翻译成字符串
+ *
+ * @author yanzm
+ * @since 2020/6/9
+ */
+public class TranslateNum {
+
+
+    public int translateNum(int num) {
+        int fst = num % 10;
+        int numAfterFst = num / 10;
+        if (numAfterFst == 0) {
+            return 1;
+        }
+        int sec = numAfterFst % 10;
+        int secFst = sec * 10 + fst;
+        if (sec > 0 && secFst <= 25) {
+            int numAfterSecFst = numAfterFst / 10;
+            if (numAfterSecFst == 0) {
+                return this.translateNum(numAfterFst) + 1;
+            }
+            return this.translateNum(numAfterSecFst) + this.translateNum(numAfterFst);
+        }
+        return this.translateNum(numAfterFst);
+    }
+}
---
+++ src/test/java/org/stray/leet/code/offer/TranslateNumTest.java
@@ -0,0 +1,39 @@
+package org.stray.leet.code.offer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author yanzm
+ * @since 2020/6/9
+ */
+class TranslateNumTest {
+
+    private final TranslateNum translateNum = new TranslateNum();
+
+    @Test
+    void translateNum() {
+        int result = translateNum.translateNum(12258);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    void translateNum2() {
+        int result = translateNum.translateNum(506);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void translateNum3() {
+        int result = translateNum.translateNum(12);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void translateNum4() {
+        int result = translateNum.translateNum(122);
+        Assertions.assertEquals(3, result);
+    }
+}
